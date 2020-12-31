5f0e8396b8b7a2f03256587126314fb7a68acb91
lichunle <lichunle@haier.com>
2020-12-31 09:36:07 +0800

    测试类修改

--- src/test/java/com/example/demo/TestDemo.java
+++ src/test/java/com/example/demo/TestDemo.java
@@ -1,8 +1,13 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class TestDemo {
 
+    @Test
+    public void mapTest() {
+        System.out.println("===========");
+    }
 }
